2a765ce2a8a4e0e1ca77bec4f353dc762162e295
proactive-analyst-6157 <u.batsaikhan@gmail.com>
2020-04-18 03:19:05 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -7,7 +7,7 @@ class Shoe
 
   def initialize(brand)
     @brand = brand
-    BRANDS.detect do {|element| element == @brand} 
+    BRANDS.detect {|element| element == @brand} 
     BRANDS << @brand
   end
 
